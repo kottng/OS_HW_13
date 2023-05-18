874aab97dd89085461975fdb105c517f79b9e4f2
Dmitriy Pavlovich <75154790+kottng@users.noreply.github.com>
2023-05-18 18:56:58 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,17 +6,17 @@
 
 int main(void) {
         int depth = 0;
-        char *soruce_file = "a";
+        char *source_file = "a";
         char *dir = "dir1";
         FILE *file;
         char symlink_elem[3];
         mkdir(dir, 0700);
-        file = fopen(soruce_file, "w");
+        file = fopen(source_file, "w");
         fclose(file);
         for (; depth < 1000; ++depth) {
                 char elem1 = 'a' + depth;
                 snprintf(symlink_elem, 3, "%c%c", elem1, elem1);
-                symlink(soruce_file, symlink_elem);
+                symlink(source_file, symlink_elem);
                 if (fopen(symlink_elem, "r") == NULL) {
                     printf("Достигнута глубина рекурсии: %d\n", depth);
                     break;
@@ -29,7 +29,7 @@ int main(void) {
                 snprintf(symlink_elem, 3, "%c%c", elem1, elem1);
                 unlink(symlink_elem);
         }
-	unlink(soruce_file);
+	unlink(source_file);
 	rmdir(dir);
 	return 0;
 }
